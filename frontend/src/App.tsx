e14361984606ded5f886dc197533a0cdfd1d275c
bitofsomething <joerachas@gmail.com>
2026-02-02 02:53:28 -0500

    login page draft

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from '@/components/ui/Navbar/Navbar'
 import Home from '@/pages/Home/Home'
+import Login from '@/pages/Login/Login'
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </BrowserRouter>
