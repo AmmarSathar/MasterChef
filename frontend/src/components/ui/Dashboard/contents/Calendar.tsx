3ad9ac21fdb7a4cd4f9cda786a78d970979878e8
Nickeldon <50726216+Nickeldon@users.noreply.github.com>
2026-04-10 14:38:47 -0400

    Code cleanup

--- frontend/src/components/ui/Dashboard/contents/Calendar.tsx
+++ frontend/src/components/ui/Dashboard/contents/Calendar.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useMemo, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { AnimatePresence, motion } from "framer-motion";
 
 import { CalendarDayView } from "./calendar/CalendarDayView";
